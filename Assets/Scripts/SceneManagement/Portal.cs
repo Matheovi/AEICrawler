18195e227c51a936c4befd81b80553dcbf5d0ff9
Matheovi <wiatrok35@gmail.com>
2022-08-09 14:56:12 +0200

    added fading animation between floors

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -6,26 +6,35 @@ using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour, IPlayerTriggerable
 {
-
+    public static bool isLoading = false;
     [SerializeField] int sceneToLoad = -1;
     [SerializeField] Transform spawnPoint;
     [SerializeField] int amountOfPointsToGo = -1;
+    Animator fader;
     public void OnTrigger(PlayerController player)
     {
-        if(amountOfPointsToGo <= PlayerStats.Instance.ECTS)
-            StartCoroutine(SwitchScene(player));
-        else Debug.Log("Not enough ECTS");
+        if (amountOfPointsToGo <= PlayerStats.Instance.ECTS)
+            if (!isLoading)
+            {
+                isLoading = true;
+                StartCoroutine(SwitchScene(player));
+            }
+            else Debug.Log("Not enough ECTS");
     }
 
 
     IEnumerator SwitchScene(PlayerController player)
     {
         DontDestroyOnLoad(gameObject);
+        fader = GameObject.Find("LevelChanger").GetComponent<Animator>();
+        fader.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(1f);
         var destination  = FindObjectsOfType<Portal>().First(x=>x!=this);
         player.transform.position = destination.spawnPoint.position;
-
+        fader.ResetTrigger("FadeOut");
+        
+        isLoading = false;
         yield return SceneManager.LoadSceneAsync(sceneToLoad);
-
         Destroy(gameObject);
     }
 }
