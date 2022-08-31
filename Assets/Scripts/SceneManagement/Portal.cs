fa3601fb810b6b663256a398d469e105fbe60c94
Matheovi <wiatrok35@gmail.com>
2022-08-31 15:09:34 +0200

    Add animations to battle, add simple first moves and attacks, add attack images

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,40 +1,49 @@
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour, IPlayerTriggerable
 {
-    public static bool isLoading = false;
     [SerializeField] int sceneToLoad = -1;
     [SerializeField] Transform spawnPoint;
     [SerializeField] int amountOfPointsToGo = -1;
-    Animator fader;
+    bool isLoading = false;
+    Fader fader;
+    PlayerController player;
+    private void Start()
+    {
+       fader =  FindObjectOfType<Fader>();
+    }
     public void OnTrigger(PlayerController player)
     {
-        if (amountOfPointsToGo <= PlayerStats.Instance.ECTS)
-            if (!isLoading)
-            {
-                isLoading = true;
-                StartCoroutine(SwitchScene(player));
-            }
-            else Debug.Log("Not enough ECTS");
+        this.player = player;
+        if ((amountOfPointsToGo <= PlayerStats.Instance.ECTS) && !isLoading)
+        { 
+            isLoading = !isLoading;
+            StartCoroutine(SwitchScene(player));
+        }
+        else Debug.Log("Not enough ECTS");
     }
 
 
     IEnumerator SwitchScene(PlayerController player)
     {
         DontDestroyOnLoad(gameObject);
-        fader = GameObject.Find("LevelChanger").GetComponent<Animator>();
-        fader.SetTrigger("FadeOut");
-        yield return new WaitForSeconds(1f);
-        var destination  = FindObjectsOfType<Portal>().First(x=>x!=this);
-        player.transform.position = destination.spawnPoint.position;
-        fader.ResetTrigger("FadeOut");
-        
-        isLoading = false;
+        yield return fader.FadeIn(0.5f);
+        Debug.Log("dupaaaa");
         yield return SceneManager.LoadSceneAsync(sceneToLoad);
+        
+        var destination  = FindObjectsOfType<Portal>().First(x=>x!=this);
+        player.transform.position = destination.SpawnPoint.position;
+
+        yield return fader.FadeOut(0.5f);
+
+
+
+        isLoading = !isLoading;
         Destroy(gameObject);
     }
+
+    public Transform SpawnPoint => spawnPoint;
 }
